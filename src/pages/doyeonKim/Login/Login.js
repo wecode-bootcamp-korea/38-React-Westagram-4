00e821e1b95a8142bd2c648f20ca887a9692c093
Eunji Kang <etothej@naver.com>
2022-10-06 15:12:34 +0900

    Add: 초기세팅 완료

--- src/pages/doyeonKim/Login/Login.js
+++ src/pages/doyeonKim/Login/Login.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const LoginDoyeon = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div />;
 };
 
 export default LoginDoyeon;
--- src/pages/jinhyeokLee/Main/Main.js
+++ src/pages/jinhyeokLee/Main/Main.js
@@ -1,11 +1,7 @@
 import React from 'react';
 
 const MainJinhyeok = () => {
-  return (
-    <div>
-      
-    </div>
-  );
+  return <div />;
 };
 
 export default MainJinhyeok;
