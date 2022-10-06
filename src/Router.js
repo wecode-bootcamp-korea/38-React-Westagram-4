98cde085818a412e6c3d6502d37ffa4d077d1c3f
Eunji Kang <etothej@naver.com>
2022-10-06 15:18:14 +0900

    Add: 초기 세팅 완료

--- src/Router.js
+++ src/Router.js
@@ -1,22 +1,23 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import LoginDoyeon from "./pages/doyeonKim/Login/Login";
-import LoginEunji from "./pages/eunjiKang/Login/Login";
-import LoginJinhyeok from "./pages/jinhyeokLee/Login/Login";
-import MainDoyeon from "./pages/doyeonKim/Main/Main";
-import MainEunji from "./pages/eunjiKang/Main/Main";
-import MainJinhyeok from "./pages/jinhyeokLee/Main/Main";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import LoginDoyeon from './pages/doyeonKim/Login/Login';
+import LoginEunji from './pages/eunjiKang/Login/Login';
+import LoginJinhyeok from './pages/jinhyeokLee/Login/Login';
+import MainDoyeon from './pages/doyeonKim/Main/Main';
+import MainEunji from './pages/eunjiKang/Main/Main';
+import MainJinhyeok from './pages/jinhyeokLee/Main/Main';
+import './styles/reset.scss';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login-doyeon" element={<LoginDoyeon />}/>
+        <Route path="/login-doyeon" element={<LoginDoyeon />} />
         <Route path="/login-eunji" element={<LoginEunji />} />
-        <Route path="/login-jinhyeok" element={<LoginJinhyeok />}/>
-        <Route path="/main-doyeon" element={< MainDoyeon/>}/>
-        <Route path="/main-eunji" element={< MainEunji/>}/>
-        <Route path="/main-jinhyeok" element={< MainJinhyeok/>}/>
+        <Route path="/login-jinhyeok" element={<LoginJinhyeok />} />
+        <Route path="/main-doyeon" element={<MainDoyeon />} />
+        <Route path="/main-eunji" element={<MainEunji />} />
+        <Route path="/main-jinhyeok" element={<MainJinhyeok />} />
       </Routes>
     </BrowserRouter>
   );
